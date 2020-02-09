7e85689afe30f9e542d1cd882b9b5598655e9749
makeupyour_mind <nikitosnov@gmail.com>
2020-02-09 12:48:09 +0200

    added minify-html

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,42 @@
-const gulp = require('gulp');
+const { task, src, dest, watch, series, parallel } = require('gulp');
 const sass = require('gulp-sass');
 const jade = require('gulp-jade');
+const htmlmin = require('gulp-htmlmin');
 const del = require('del');
 
-gulp.task('clear', async function (done) {
+task('clear', async function (done) {
     return await del('build')
 })
 
-gulp.task('templates', function () {
-    return gulp.src(['src/templates/**/*.jade'])
+task('templates', function () {
+    return src(['src/templates/**/*.jade'])
         .pipe(jade())
-        .pipe(gulp.dest('src/html'))
+        .pipe(dest('src/html'))
 });
 
-gulp.task('build', function (cb) {
-    return gulp.src(['src/**/*.js', 'src/**/*.css', 'src/**/*.html', 'src/**/*.{png,gif,jpg,jpeg}'])
-        .pipe(gulp.dest('build'))
+task('minify-html', function () {
+    return src('src/**/*.html') // указываем пути к файлам .html
+        .pipe(htmlmin({
+            collapseWhitespace: true, // удаляем все переносы
+            removeComments: true // удаляем все комментарии
+        }))
+        .pipe(dest('build')); // оптимизированные файлы .html переносим на продакшен
 });
 
-gulp.task('sass', function () { // Создаем таск "sass"
-    return gulp.src(['src/sass/**/*.sass', 'src/sass/**/*.scss']) // Берем источник
+task('ready-for-production', function (cb) {
+    return src(['src/**/*.js', 'src/**/*.css', 'src/**/*.{png,gif,jpg,jpeg}'])
+        .pipe(dest('build'))
+});
+
+task('sass', function () { // Создаем таск "sass"
+    return src(['src/sass/**/*.sass', 'src/sass/**/*.scss']) // Берем источник
         .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError)) // Преобразуем Sass в CSS посредством gulp-sass
-        .pipe(gulp.dest('src/css')) // Выгружаем результата в папку css
+        .pipe(dest('src/css')) // Выгружаем результата в папку css
 });
 
-gulp.task('watch', function () {
-    gulp.watch(['src/sass/**/*.sass', 'src/sass/**/*.scss'], gulp.series('sass')); // Наблюдение за sass файлами в папке sass
+task('watch', function () {
+    watch(['src/sass/**/*.sass', 'src/sass/**/*.scss'], series('sass')); // Наблюдение за sass файлами в папке sass
 });
 
-gulp.task('default', gulp.parallel('watch'));
-gulp.task('prod', gulp.series('sass', 'clear', 'build'))
+task('default', parallel('watch'));
+task('prod', series('clear', 'minify-html', 'ready-for-production'))
